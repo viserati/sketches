13c76dd232b13f830b32c25e7558be53beb0e4ec
Keith Turner <turner@keithturner.com>
2018-04-08 08:59:03 -0600

    Generater Comment model, controller, add form on show page for comments and output

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :articles
+  resources :articles do
+  resources :comments
+  end
+
   root 'welcome#index'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
