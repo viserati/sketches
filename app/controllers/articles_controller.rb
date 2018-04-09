07569cbda29b666cbdd209d29d1b13332bec277f
Keith Turner <turner@keithturner.com>
2018-04-09 10:01:00 -0600

    Added validations to create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ def index
 end
 
 def new
+  @article = Article.new
 end
 
 def show
@@ -26,10 +27,13 @@ end
 
 
 def create
-  # render plain: params[:article].inspect
+
   @article = Article.new(article_params)
-  @article.save
+  if @article.save
   redirect_to @article
+  else
+  render 'new'
+  end
 end
 
 def destroy
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+            length: {minimum: 3}
+  validates :text, presence: true,
+            length: {minimum: 5}
 
 end
