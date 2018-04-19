fa36ee4f6579a3e994575efe16aea7e090e9c74b
Keith Turner <turner@keithturner.com>
2018-04-18 20:00:18 -0600

    Revised config/routes- fine tuning.

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ end
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route- maps HTTP verbs to controller actions automatically:
   #   resources :products
 
   # Example resource route with options:
