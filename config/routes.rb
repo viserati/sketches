18668a9d70814db983eaf7c1270bb3190750e3a3
Keith Turner <turner@keithturner.com>
2018-04-26 22:14:48 -0600

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :articles do
   resources :comments
   collection do
-    get :search #creates a new path for the searching
+    get :search #creates a new path for searching
   end
   end
   resources :subscribers
