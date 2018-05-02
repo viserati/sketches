8aeffe99ce193a5a210320ee0890515ab04fdea3
Keith Turner <turner@keithturner.com>
2018-05-01 21:43:58 -0600

    Implemented authorization stage 21 for creating an account.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_uniqueness_of :email
+
+  # Create SESSIONS for active user and terminate on signing out.
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   get 'welcome/index'
   resources :articles do
